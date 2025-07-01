1aeb6a4514a56c7f0a9befe8ec2f7deb9c7f5794
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-07-01 21:16:10 +0900

    fixed: Corrected `uv` upgrade script. chore: Upgraded dependencies. chore: Upgraded Smithery dockerfile to use requirements.txt

--- uv-upgrade.sh
+++ uv-upgrade.sh
@@ -3,6 +3,5 @@
 uv lock -U
 # Export the requirements to a requirements.txt file
 uv export --no-hashes --format requirements-txt > requirements.txt
-# Run the tests and report coverage with missing values
-uv run --group test coverage run -m pytest tests/
-uv run coverage report -m
+# Run tests using pytest
+uv run --group test pytest tests/
